a050dc91985f4aa7a0b12d1a15277b5235a0fa32
Manoj Pentapalli <mkpentapalli2k2@gmail.com>
2024-03-11 05:53:03 +0000

    Added gcloud functions

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, File, UploadFile, Request, Form
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
 import os
 from dotenv import load_dotenv
 from mail_gen import EmailGenFromGemini 
+from google.cloud import storage
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, File, UploadFile, Request, Form
 app = FastAPI()
 
 # Define folder paths
@@ -18,6 +19,64 @@ os.makedirs(target_person_folder, exist_ok=True)
 
 # Initialize templates
 templates = Jinja2Templates(directory="templates")
+
+# Defining the variables for google-cloud
+key_path = "/workspace/gcp_service_account.json"
+project_id = "ck-eams"
+bucket_name = "email_gen_llm"
+upload_company_folder = "Company_Data/"
+upload_person_folder = "Person_Data/"
+storage_Client = storage.Client.from_service_account_json(key_path)
+
+
+
+# Checking if the service account is correctly authenticated or not
+def authenticate() -> bool:
+    try:
+        temp_project_id = storage_Client.project
+        return temp_project_id == project_id
+    except Exception(e):
+        print(f"Exception:\n{e}")
+        return False
+
+
+
+# # get_file_name
+# def get_file_name(filepath : str = None) -> str:
+#     return filepath.split('/')[-1]
+
+
+
+# change_file_name
+def change_file_name(filepath : str = None) -> None:
+    original_file_path = filepath
+    file_list = filepath.split('/')
+    file_list[-1] = file_list[-1].lower().replace(" ","_")
+    result_path = ""
+    for string in file_list:
+        result_path += string
+        result_path += "/"
+    result_path = result_path[0:-1]
+    os.rename(original_file_path,result_path)
+
+
+
+# Function to check if upload file exists in cloud bucket or not:
+def check_file_exists(folder_name : str = None, file_name : str = None) -> bool:
+    bucket = storage_Client.get_bucket("email_gen_llm")
+    blob = bucket.blob(f"{folder_name}{file_name}")
+    if blob:
+        return True
+    return False
+
+
+
+# Upload file to the bucket:
+def upload_file(folder_name : str = None, file_path : str = None):
+    file_name = file_path.split('/')[-1]
+    bucket = storage_Client.get_bucket("email_gen_llm")
+    blob = bucket.blob(f"{folder_name}{file_name}")
+    blob.upload_from_filename(file_path)
 
 
 
@@ -48,16 +107,18 @@ async def upload_files(request: Request,
     # Save target person file
     with open(target_person_path, "wb") as buffer:
         buffer.write(await targetPerson.read())
+
     
-    # Now you can access the selected tone using the 'tone' variable
-    print("Selected tone:", tone)
-    text="Hi!!!!!!"
+    
+    # # Now you can access the selected tone using the 'tone' variable
+    # print("Selected tone:", tone)
+    # text="Hi!!!!!!"
     mail_gen_instance = EmailGenFromGemini(model_name = 'gemini-pro',
                  source_path = product_path,
                  target_company_path = target_company_path,
                  target_person_path = target_person_path)
     mail = mail_gen_instance.email_generation()
-    print(mail)
+    # print(mail)
     html_response = f"<!DOCTYPE html>\
     <html>\
     <head>\
@@ -74,5 +135,5 @@ async def upload_files(request: Request,
     </body>\
     </html>"
     
-    print(mail['email'])
+    # print(mail['email'])
     return templates.TemplateResponse("index.html", {"request": request, "message":  "Files uploaded successfully","text":mail['email']})
